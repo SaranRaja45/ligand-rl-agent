f973db29e8b12273b548ce4898dc2603063cccf4
SaranRaja45 <163095125+SaranRaja45@users.noreply.github.com>
2026-04-08 23:51:15 +0530

    Add files via upload

---
+++ server/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from run_baseline import run_all_tasks
+
+app = FastAPI()
+
+@app.post("/reset")
+def reset():
+    t1, t2, t3 = run_all_tasks()
+    episodes = 30
+
+    return{
+        "Task1": round(t1 / episodes, 2),
+        "Task2": round(t2 / episodes, 2),
+        "Task3": round(t3 / episodes, 2)
+    }
+
+@app.get("/")
+def root():
+    return{"status": "HM-Dock API working"}
